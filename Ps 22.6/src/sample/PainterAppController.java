2dfa1288c8f6aafe15221ac28fea145aaba9d47a
hakimahmadi <hakimsshah@gmail.com>
2021-03-17 21:17:38 +0600

    Updated

--- Ps 22.6/src/sample/PainterAppController.java
+++ Ps 22.6/src/sample/PainterAppController.java
@@ -1,206 +1,241 @@
 package sample;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.*;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.control.RadioButton;
 
 import java.util.Stack;
 
-
 public class PainterAppController {
 
-
-    @FXML
-    private Slider redSlider;
-    @FXML
-    private Slider greenSlider;
-    @FXML
-    private Slider blueSlider;
-    @FXML
-    private Slider alphaSlider;
-    @FXML
-    private TextField redTextField;
-    @FXML
-    private TextField greenTextField;
-    @FXML
-    private TextField blueTextField;
-    @FXML
-    private TextField alphaTextField;
-    @FXML
-    private Rectangle colorRectangle;
-    @FXML
-    private Button undoButton;
-    @FXML
-    private Button clearButton;
-    @FXML private Canvas drawingAreaCanvas;
-
-    @FXML private RadioButton lineButton;
-    @FXML private RadioButton ovalButton;
-    @FXML private RadioButton RectangleButton;
-    @FXML private RadioButton EraserButton;
-    @FXML private Slider thicknessSlider;
+    @FXML    private ToggleButton drowbtn;
+    @FXML    private ToggleButton rubberbtn;
+    @FXML    private ToggleButton linebtn;
+    @FXML    private ToggleButton rectbtn;
+    @FXML    private ToggleButton circlebtn;
+    @FXML    private Label Borderclr;
+    @FXML    private ColorPicker cpLine;
+    @FXML    private ColorPicker cpFill;
+    @FXML    private Slider slider;
+    @FXML    private Button undo;
+    @FXML    private Button clearButton;
+    @FXML    private Canvas canvas;
 
 
+    public void initialize(){
 
-    @FXML
-    GraphicsContext gc = drawingAreaCanvas.getGraphicsContext2D();
+        Stack<Shape> undoHistory = new Stack();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-    private int red = 0;
-    private int green = 0;
-    private int blue = 0;
-    private double alpha = 1.0;
-    Line line = new Line();
-    Rectangle rect = new Rectangle();
-    Ellipse oval = new Ellipse();
-    Stack<Shape> history = new Stack();
+        cpLine.setValue(Color.BLACK);
 
+        gc.setLineWidth(1);
 
-    private Paint brushColor = Color.BLACK;
+        Line line = new Line();
+        Rectangle rect = new Rectangle();
+        Circle circ = new Circle();
 
-
-    public void initialize() {
-
-        redTextField.textProperty().bind(
-                redSlider.valueProperty().asString("%.0f"));
-        greenTextField.textProperty().bind(
-                greenSlider.valueProperty().asString("%.0f"));
-        blueTextField.textProperty().bind(
-                blueSlider.valueProperty().asString("%.0f"));
-        alphaTextField.textProperty().bind(
-                alphaSlider.valueProperty().asString("%.2f"));
-
-        drawingAreaCanvas.setOnMousePressed(e -> {
-            if (lineButton.isSelected()) {
+        canvas.setOnMousePressed(e->{
+            if(drowbtn.isSelected()) {
+                gc.setStroke(cpLine.getValue());
+                gc.beginPath();
+                gc.lineTo(e.getX(), e.getY());
+            }
+            else if(rubberbtn.isSelected()) {
+                double lineWidth = gc.getLineWidth();
+                gc.clearRect(e.getX() - lineWidth / 2, e.getY() - lineWidth / 2, lineWidth, lineWidth);
+            }
+            else if(linebtn.isSelected()) {
+                gc.setStroke(cpLine.getValue());
                 line.setStartX(e.getX());
                 line.setStartY(e.getY());
-                line.setEndX(e.getX());
-                line.setEndY(e.getY());
-            } else if (RectangleButton.isSelected()) {
+            }
+            else if(rectbtn.isSelected()) {
+                gc.setStroke(cpLine.getValue());
+                gc.setFill(cpFill.getValue());
                 rect.setX(e.getX());
                 rect.setY(e.getY());
-            } else if (ovalButton.isSelected()) {
-                oval.setCenterX(e.getX());
-                oval.setCenterY(e.getY());
             }
-
-            else if (EraserButton.isSelected()) erase(e.getX(), e.getY());
+            else if(circlebtn.isSelected()) {
+                gc.setStroke(cpLine.getValue());
+                gc.setFill(cpFill.getValue());
+                circ.setCenterX(e.getX());
+                circ.setCenterY(e.getY());
+            }
 
         });
 
-        drawingAreaCanvas.setOnMouseReleased(e -> {
+        canvas.setOnMouseDragged(e->{
+            if(drowbtn.isSelected()) {
+                gc.lineTo(e.getX(), e.getY());
+                gc.stroke();
+            }
+            else if(rubberbtn.isSelected()){
+                double lineWidth = gc.getLineWidth();
+                gc.clearRect(e.getX() - lineWidth / 2, e.getY() - lineWidth / 2, lineWidth, lineWidth);
+            }
+        });
 
-            gc.setStroke(Color.rgb(red, green, blue));
-
-            if (lineButton.isSelected()) {
-                line.setStrokeWidth(thicknessSlider.getValue());
-                line.setFill(Color.rgb(red, green, blue));
+        canvas.setOnMouseReleased(e->{
+            if(drowbtn.isSelected()) {
+                gc.lineTo(e.getX(), e.getY());
+                gc.stroke();
+                gc.closePath();
+            }
+            else if(rubberbtn.isSelected()) {
+                double lineWidth = gc.getLineWidth();
+                gc.clearRect(e.getX() - lineWidth / 2, e.getY() - lineWidth / 2, lineWidth, lineWidth);
+            }
+            else if(linebtn.isSelected()) {
                 line.setEndX(e.getX());
                 line.setEndY(e.getY());
-                gc.setLineWidth(thicknessSlider.getValue());
                 gc.strokeLine(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY());
-                Line tempLine = new Line(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY());
-                tempLine.setStroke(Color.rgb(red, green, blue));
-                tempLine.setStrokeWidth(line.getStrokeWidth());
-                history.push(tempLine);
+
+                undoHistory.push(new Line(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY()));
             }
-            else if (RectangleButton.isSelected()) {
+            else if(rectbtn.isSelected()) {
                 rect.setWidth(Math.abs((e.getX() - rect.getX())));
                 rect.setHeight(Math.abs((e.getY() - rect.getY())));
-                rect.setX(Math.min(rect.getX(), e.getX()));
-                rect.setY(Math.min(rect.getY(), e.getY()));
-                rect.setFill(Color.rgb(red, green, blue));
-                gc.setFill(Color.rgb(red, green, blue));
+                if(rect.getX() > e.getX()) {
+                    rect.setX(e.getX());
+                }
+
+                if(rect.getY() > e.getY()) {
+                    rect.setY(e.getY());
+                }
+
                 gc.fillRect(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
                 gc.strokeRect(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
-                Rectangle tempRect = new Rectangle(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
-                tempRect.setFill(Color.rgb(red, green, blue));
-                history.push(tempRect);
-            } else if (ovalButton.isSelected()) {
-                oval.setRadiusX(Math.abs(e.getX() - oval.getCenterX()));
-                oval.setRadiusY(Math.abs(e.getY() - oval.getCenterY()));
-                oval.setCenterX(Math.min(oval.getCenterX(), e.getX()));
-                oval.setCenterY(Math.min(oval.getCenterY(), e.getY()));
-                oval.setFill(Color.rgb(red, green, blue));
-                gc.setFill(Color.rgb(red, green, blue));
-                gc.fillOval(oval.getCenterX(), oval.getCenterY(), oval.getRadiusX(), oval.getRadiusY());
-                gc.strokeOval(oval.getCenterX(), oval.getCenterY(), oval.getRadiusX(), oval.getRadiusY());
-                Ellipse tempOval = new Ellipse(oval.getCenterX(), oval.getCenterY(), oval.getRadiusX(), oval.getRadiusY());
-                tempOval.setFill(Color.rgb(red, green, blue));
-                history.push(tempOval);
+
+                undoHistory.push(new Rectangle(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight()));
+
+            }
+            else if(circlebtn.isSelected()) {
+                circ.setRadius((Math.abs(e.getX() - circ.getCenterX()) + Math.abs(e.getY() - circ.getCenterY())) / 2);
+
+                if(circ.getCenterX() > e.getX()) {
+                    circ.setCenterX(e.getX());
+                }
+                if(circ.getCenterY() > e.getY()) {
+                    circ.setCenterY(e.getY());
+                }
+
+                gc.fillOval(circ.getCenterX(), circ.getCenterY(), circ.getRadius(), circ.getRadius());
+                gc.strokeOval(circ.getCenterX(), circ.getCenterY(), circ.getRadius(), circ.getRadius());
+
+                undoHistory.push(new Circle(circ.getCenterX(), circ.getCenterY(), circ.getRadius()));
             }
 
-            else if (EraserButton.isSelected()) erase(e.getX(), e.getY());
-                gc.clearRect(0, 0, drawingAreaCanvas.getWidth(), drawingAreaCanvas.getHeight());
-                drawShapes(history);
+
+            Shape lastUndo = undoHistory.lastElement();
+            lastUndo.setFill(gc.getFill());
+            lastUndo.setStroke(gc.getStroke());
+            lastUndo.setStrokeWidth(gc.getLineWidth());
+
         });
 
+        cpLine.setOnAction(e->{
+            gc.setStroke(cpLine.getValue());
+        });
+        cpFill.setOnAction(e->{
+            gc.setFill(cpFill.getValue());
+        });
+
+
+        slider.valueProperty().addListener(e->{
+            double width = slider.getValue();
+            gc.setLineWidth(width);
+        });
+
+        undo.setOnAction(e->{
+            if(!undoHistory.empty()){
+                gc.clearRect(0, 0, 1080, 790);
+                Shape removedShape = undoHistory.lastElement();
+                if(removedShape.getClass() == Line.class) {
+                    Line tempLine = (Line) removedShape;
+                    tempLine.setFill(gc.getFill());
+                    tempLine.setStroke(gc.getStroke());
+                    tempLine.setStrokeWidth(gc.getLineWidth());
+
+                }
+                else if(removedShape.getClass() == Rectangle.class) {
+                    Rectangle tempRect = (Rectangle) removedShape;
+                    tempRect.setFill(gc.getFill());
+                    tempRect.setStroke(gc.getStroke());
+                    tempRect.setStrokeWidth(gc.getLineWidth());
+
+                }
+                else if(removedShape.getClass() == Circle.class) {
+                    Circle tempCirc = (Circle) removedShape;
+                    tempCirc.setStrokeWidth(gc.getLineWidth());
+                    tempCirc.setFill(gc.getFill());
+                    tempCirc.setStroke(gc.getStroke());
+
+                }
+                else if(removedShape.getClass() == Ellipse.class) {
+                    Ellipse tempElps = (Ellipse) removedShape;
+                    tempElps.setFill(gc.getFill());
+                    tempElps.setStroke(gc.getStroke());
+                    tempElps.setStrokeWidth(gc.getLineWidth());
+
+                }
+                undoHistory.pop();
+
+                for(int i=0; i < undoHistory.size(); i++) {
+                    Shape shape = undoHistory.elementAt(i);
+                    if(shape.getClass() == Line.class) {
+                        Line temp = (Line) shape;
+                        gc.setLineWidth(temp.getStrokeWidth());
+                        gc.setStroke(temp.getStroke());
+                        gc.setFill(temp.getFill());
+                        gc.strokeLine(temp.getStartX(), temp.getStartY(), temp.getEndX(), temp.getEndY());
+                    }
+                    else if(shape.getClass() == Rectangle.class) {
+                        Rectangle temp = (Rectangle) shape;
+                        gc.setLineWidth(temp.getStrokeWidth());
+                        gc.setStroke(temp.getStroke());
+                        gc.setFill(temp.getFill());
+                        gc.fillRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
+                        gc.strokeRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
+                    }
+                    else if(shape.getClass() == Circle.class) {
+                        Circle temp = (Circle) shape;
+                        gc.setLineWidth(temp.getStrokeWidth());
+                        gc.setStroke(temp.getStroke());
+                        gc.setFill(temp.getFill());
+                        gc.fillOval(temp.getCenterX(), temp.getCenterY(), temp.getRadius(), temp.getRadius());
+                        gc.strokeOval(temp.getCenterX(), temp.getCenterY(), temp.getRadius(), temp.getRadius());
+                    }
+                    else if(shape.getClass() == Ellipse.class) {
+                        Ellipse temp = (Ellipse) shape;
+                        gc.setLineWidth(temp.getStrokeWidth());
+                        gc.setStroke(temp.getStroke());
+                        gc.setFill(temp.getFill());
+                        gc.fillOval(temp.getCenterX(), temp.getCenterY(), temp.getRadiusX(), temp.getRadiusY());
+                        gc.strokeOval(temp.getCenterX(), temp.getCenterY(), temp.getRadiusX(), temp.getRadiusY());
+                    }
+                }
+            } else {
+                System.out.println("there is no action to undo");
+            }
+        });
+
+
+        clearButton.setOnAction(e-> {
+            gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            undoHistory.clear();
+
+        });
     }
-
-
-//    @FXML
-//    private void drawingAreaMouseDragged(MouseEvent e) {
-//        Circle newCircle = new Circle(e.getX(), e.getY(),
-//                2, brushColor);
-//        drawingAreaPane.getChildren().add(newCircle);
-//    }
-
 
     @FXML
-    private void undoButtonPressed(ActionEvent event) {
-        if (!history.isEmpty()) {
-            history.pop();
-            gc.clearRect(0, 0, drawingAreaCanvas.getWidth(), drawingAreaCanvas.getHeight());
-            drawShapes(history);
-        }
+    void undoButtonPressed(ActionEvent event) {
 
     }
-    private void drawShapes(Stack<Shape> shapes) {
-        for (Shape s: shapes) {
-            if(s.getClass() == Line.class) {
-                Line temp = (Line) s;
-                gc.setLineWidth(temp.getStrokeWidth());
-                gc.setStroke(temp.getStroke());
-                gc.strokeLine(temp.getStartX(), temp.getStartY(), temp.getEndX(), temp.getEndY());
-            }
-            else if(s.getClass() == Rectangle.class) {
-                Rectangle temp = (Rectangle) s;
-                gc.setLineWidth(temp.getStrokeWidth());
-                gc.setFill(temp.getFill());
-                gc.setStroke(temp.getStroke());
-                gc.fillRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
-                gc.strokeRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
-            }
-            else if(s.getClass() == Ellipse.class) {
-                Ellipse temp = (Ellipse) s;
-                gc.setStroke(temp.getStroke());
-                gc.setLineWidth(temp.getStrokeWidth());
-                gc.setFill(temp.getFill());
-                gc.fillOval(temp.getCenterX(), temp.getCenterY(), temp.getRadiusX(), temp.getRadiusY());
-                gc.strokeOval(temp.getCenterX(), temp.getCenterY(), temp.getRadiusX(), temp.getRadiusY());
-            }
-        }
-    }
 
-    private void erase(double x, double y) {
-        double sise = thicknessSlider.getValue();
-        gc.clearRect(x - sise / 2, y - sise / 2, sise, sise);
-    }
-    @FXML
-    private void clearButtonPressed(ActionEvent event) {
-        gc.clearRect(0, 0, drawingAreaCanvas.getWidth(), drawingAreaCanvas.getHeight());
-        history.clear();
-    }
+
 }

8545316b524edbfd759a04e04b5274ff135de03b
hakimahmadi <hakimsshah@gmail.com>
2021-04-07 23:46:04 +0600

    Updated

--- PS 22.22 TextShadow/src/sample/Controller.java
+++ PS 22.22 TextShadow/src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML private Label TextLabel;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setRadius(5.0);
+        dropShadow.setOffsetX(3.0);
+        dropShadow.setOffsetY(3.0);
+        dropShadow.setColor(Color.color(0.4, 0.5, 0.5));
+
+        TextLabel.setText("JavaFX");
+        TextLabel.setEffect(dropShadow);
+    }
 }

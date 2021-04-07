77451412f9e2f36a3303b095896492c1a1943391
hakimahmadi <hakimsshah@gmail.com>
2021-04-08 00:32:59 +0600

    updated

--- PS 22.24 Shadow/src/sample/Controller.java
+++ PS 22.24 Shadow/src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
-public class Controller {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML private Rectangle Rectangle;
+
+    @FXML private Slider slider;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DropShadow ds = new DropShadow();
+
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<?extends Number> observable, Number oldValue, Number newValue){
+                ds.setOffsetY(newValue.floatValue());
+                ds.setOffsetX(newValue.floatValue());DropShadow ds = new DropShadow();
+                ds.setColor(Color.GRAY);
+                Rectangle.setEffect(ds);
+            }
+        });
+
+
+
+
+    }
 }

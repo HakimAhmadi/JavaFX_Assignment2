eaefd9a1a83a397ae5f950948e43629f9e1d550a
hakimahmadi <hakimsshah@gmail.com>
2021-05-16 17:18:43 +0600

    Update

--- Ps 22.7/src/sample/PainterAppPane.java
+++ Ps 22.7/src/sample/PainterAppPane.java
@@ -1,22 +1,22 @@
 package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class PainterApp extends Application {
-    @Override
-    public void start(Stage Stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("PainterApp.fxml"));
-        Scene scene = new Scene(root);
-        Stage.setTitle("Painter App");
-        Stage.setScene(scene);
-        Stage.show();
-    }
 
+public class PainterAppPane extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("PainterAppPane.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Static Art App");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
         launch(args);
     }
 }
+
